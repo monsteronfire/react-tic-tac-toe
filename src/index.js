59cacb65c7a2420e77d65ad43fdcd5f8c55842e6
Shaii Ong <monsteronfire@gmail.com>
2017-07-02 14:31:16 +0800

    Add boilerplate starter app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.js'
 require('./index.css');
+
+ReactDOM.render(<App/>, document.getElementById('root'));
