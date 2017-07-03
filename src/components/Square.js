6ed3f013c40e38b348a2feec739532ed8b8a8016
Shaii Ong <monsteronfire@gmail.com>
2017-07-03 20:57:03 +0800

    Add code for starter components

---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Square extends React.Component {
+  render() {
+    return (
+      <div className='square'>
+        { /* TODO */ }
+      </div>
+    )
+  }
+}
+
+export default Square;
