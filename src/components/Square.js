ce1f12c6901d4fe1edbda642510d8350e9a43bb1
Shaii Ong <monsteronfire@gmail.com>
2017-07-03 21:24:26 +0800

    Add X when button clicked

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <div className='square'>
-        { /* TODO */ }
-      </div>
+      <button className='square' onClick={ () => this.setState({value: 'X'}) }>
+        {this.state.value}
+      </button>
     )
   }
 }
